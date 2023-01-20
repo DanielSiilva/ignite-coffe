6b341049b32b598bf5683779efef504f6bdcfadf
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-01-19 23:14:54 -0300

    Feat: iniciando home - estilização:titulo, subtitulo e banner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,51 @@
+import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react";
+import {Wrapper, Information, TitleSubtitle, Highlights, Item01,Item02,Item03, Item04, Logo} from "./styled"
 
+
+
+
+import Banner from "../../assets/banner.svg"
 
 
 export function Home (){
 
     return(
-        <>
-            <h1>Home</h1>
-        </>
+        <Wrapper>
+            <Information>
+                <TitleSubtitle>
+                    <h1>
+                        Encontre o café perfeito para qualquer hora do dia
+                    </h1>
+
+                    <p>
+                        Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora
+                    </p>
+                </TitleSubtitle>
+
+                <Highlights>
+                   <Item01>
+                        <ShoppingCart size={22} weight='fill' />
+                        <p> Compra simples e segura</p>
+                   </Item01>
+                    
+                   <Item02>
+                        <Package size={22} weight='fill'/>
+                        <p> Embalagem mantém o café intacto</p>
+                   </Item02>
+
+                   <Item03>
+                        <Timer size={22}  weight='fill'/>
+                        <p> Entrega rápida e rastreada</p>
+                   </Item03>
+
+                   <Item04>
+                        <Coffee size={22} weight='fill' />
+                        <p> O café chega fresquinho até você</p>
+                   </Item04>
+                </Highlights>
+            </Information>
+
+            <Logo  src={Banner}/>
+        </Wrapper>
     )
 }
--- src/pages/Home/styled.ts
+++ src/pages/Home/styled.ts
@@ -1,1 +1,109 @@
 import styled from "styled-components";
+
+
+export const Wrapper =styled.div`
+    display: flex;
+    gap: 1rem;
+    padding: 2rem 10rem;
+
+`
+
+export const Information = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+
+`
+
+export const TitleSubtitle = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+
+    h1{
+        font-family: 'Baloo 2';
+        font-style: normal;
+        font-weight: 800;
+        font-size: 2.8rem;
+        line-height: 130%;
+     
+        color: ${props => props.theme['base-title']};
+
+    }
+
+    p{
+  
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 1.3rem;
+        line-height: 130%;
+       
+        color: ${props => props.theme['base-subtitle']};
+        font-stretch: 100;
+    }
+
+`
+
+const BaseItem = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    p{
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 1rem;
+        white-space: nowrap;
+
+        color: ${props => props.theme['base-text']};
+    }
+
+    svg{
+        padding: 6px;
+        color: ${props => props.theme['background']};
+        border-radius:100%;
+        width: 1.8rem;
+        height: 1.8rem;
+    }
+`
+
+export const Highlights = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap:1rem;
+`
+
+export const Item01 = styled(BaseItem)`
+    svg{
+        background: ${props => props.theme['purple']};
+    }
+`
+
+export const Item02 = styled(BaseItem)`
+    svg{
+        background: ${props => props.theme['base-text']};
+    }
+
+`
+
+export const Item03 = styled(BaseItem)`
+    svg{
+        background: ${props => props.theme['yellow']};
+    }
+
+`
+
+export const Item04 = styled(BaseItem)`
+    svg{
+        background: ${props => props.theme['purple']};
+    }
+
+`
+
+export const Logo = styled.img`
+    width: 470px;
+    height: 330px;  
+
+`
