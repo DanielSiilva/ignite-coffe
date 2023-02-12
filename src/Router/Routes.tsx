1699011c5f78083cdc7a1d78bca094b2a6d5a6d8
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-11 22:43:34 -0300

    Feat: adcionando pagina de sucesso no Router

--- src/Router/Routes.tsx
+++ src/Router/Routes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "../layouts/DefaultLayout";
 import { Checkout } from "../pages/CheckoutFilled";
 import { Home } from "../pages/Home";
+import { SucessOrder } from "../pages/SuccessOrder";
 
 
 
@@ -14,6 +15,7 @@ export function Router (){
             <Route path="/" element ={<DefaultLayout/>}>
                 <Route  path="/" element ={<Home/>} />
                 <Route path="/coffee-delivery/checkout" element ={<Checkout />} />
+                <Route path="/coffee-delivery/success" element ={<SucessOrder />} />
             </Route>
 
         </Routes>
