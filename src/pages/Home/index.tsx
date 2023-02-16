22c335084c79712de9d04037f9c2292300c89fb2
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-16 13:01:20 -0300

    Remove: remove importações de context

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,8 +5,6 @@ import {Wrapper} from "./styled"
 
 import { Information } from "./Information"
 import { CoffeeList } from "./CoffeeList"
-import { useContext } from "react"
-import { CoffeeShopContext } from "../../context/CartContext"
 
 
 export function Home (){
